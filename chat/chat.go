8996d911cddc76d5f67f2446c581c63f6e9749d6
UrTeddy on drugs <trad.aidoud@gmail.com>
2023-10-24 22:01:15 +0200

    refactor(client-server): add interfaces

---
+++ chat/chat.go
@@ -0,0 +1,73 @@
+package chat
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net"
+)
+
+type Message struct {
+	Sender string `json:"sender"`
+	Text   string `json:"text"`
+	IChat
+}
+
+type Client struct {
+	Conn     net.Conn
+	Messages chan Message
+}
+
+type Disconnect struct {
+	Client Client
+}
+
+type IChat interface {
+	DispatchMessage()
+	EncodeMessage(sender, text string) ([]byte, error)
+	SendMessage(conn io.Writer, message []byte) error
+	ReceiveMessage(conn io.Reader) (string, error)
+	DecodeMessage(message string) (Message, error)
+}
+
+func (c *Client) EncodeMessage(sender, text string) ([]byte, error) {
+	message := Message{
+		Sender: sender,
+		Text:   text,
+	}
+
+	messageJSON, err := json.Marshal(message)
+	if err != nil {
+		return nil, err
+	}
+
+	return append(messageJSON, '\n'), nil
+}
+
+func (c *Client) SendMessage(conn io.Writer, message []byte) error {
+	_, err := conn.Write(message)
+	if err != nil {
+		fmt.Println("Error while sending message")
+		return err
+	}
+	return nil
+}
+
+func (c *Client) ReceiveMessage(conn io.Reader) (string, error) {
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	message := string(buffer[:n])
+	return message, nil
+}
+
+func (c *Client) DecodeMessage(messageData string) (Message, error) {
+	var receivedMessage Message
+	if err := json.Unmarshal([]byte(messageData), &receivedMessage); err != nil {
+		return Message{}, err
+	}
+	return receivedMessage, nil
+}
--- client.go
+++ client.go
@@ -1,21 +1,18 @@
 package main
 
 import (
-	"bufio"
-	"encoding/json"
+	"deskor/chat"
 	"fmt"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 	"github.com/joho/godotenv"
 	"log"
 	"net"
 	"os"
 	"os/signal"
-	"strings"
 )
-
-type Message struct {
-	Sender string `json:"sender"`
-	Text   string `json:"text"`
-}
 
 func main() {
 	err := godotenv.Load(".env.client")
@@ -32,62 +29,71 @@ func main() {
 	}
 	defer conn.Close()
 
-	fmt.Print("Type your username: ")
-	username, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	username = strings.TrimSpace(username)
-
-	fmt.Println("Connected")
-
 	exit := make(chan os.Signal, 1)
 	signal.Notify(exit, os.Interrupt)
 
+	myApp := app.New()
+	window := myApp.NewWindow("Deskor")
+	usernameWidget := widget.NewEntry()
+	messageWidget := widget.NewEntry()
+	chatWidget := widget.NewLabel("Chat will appear here")
+
+	chatScroller := container.NewVScroll(chatWidget)
+
+	usernameWidget.SetPlaceHolder("Enter your username")
+	messageWidget.SetPlaceHolder("Type your message and press Enter")
+
+	messageWidget.OnSubmitted = func(text string) {
+		sender := usernameWidget.Text
+		chater := &chat.Client{}
+
+		message, err := chater.EncodeMessage(sender, text)
+		if err != nil {
+			fmt.Println("Error while encoding message")
+			close(exit)
+		} else {
+			err = chater.SendMessage(conn, message)
+			if err != nil {
+				fmt.Println("Error while sending message")
+				close(exit)
+			}
+		}
+		messageWidget.SetText("")
+	}
+
 	go func() {
 		for {
-			buffer := make([]byte, 1024)
-			n, err := conn.Read(buffer)
+			chater := &chat.Client{}
+
+			message, err := chater.ReceiveMessage(conn)
 			if err != nil {
-				fmt.Println("Error while reading messages:")
+				fmt.Println("Error while reading messages:", err)
 				close(exit)
 				break
 			}
-			message := string(buffer[:n])
 
-			var receivedMessage Message
-			if err := json.Unmarshal([]byte(message), &receivedMessage); err == nil {
-				fmt.Printf("\033[1A\033[K")
-				fmt.Printf("%s: %s", receivedMessage.Sender, receivedMessage.Text)
+			var receivedMessage chat.Message
+
+			if decodedMessage, err := chater.DecodeMessage(message); err == nil {
+				receivedMessage = decodedMessage
 			} else {
-				// TODO: idk in which case it's possible, and how to handle it
-				fmt.Printf("\033[1A\033[K")
-				fmt.Print(message)
+				fmt.Println("Error while decoding message:", err)
 			}
+
+			chatWidget.SetText(chatWidget.Text + "\n" + receivedMessage.Sender + ": " + receivedMessage.Text)
+			chatScroller.ScrollToBottom()
 		}
 	}()
 
-	go func() {
-		for {
-			text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	content := container.NewBorder(
+		usernameWidget,
+		messageWidget,
+		nil,
+		nil,
+		chatScroller,
+	)
 
-			message := Message{
-				Sender: username,
-				Text:   text,
-			}
-
-			messageJSON, err := json.Marshal(message)
-			if err != nil {
-				fmt.Println("Error while sending message")
-				close(exit)
-				break
-			}
-			_, err = conn.Write(append(messageJSON, '\n'))
-			if err != nil {
-				fmt.Println("Error while sending message")
-				close(exit)
-				break
-			}
-		}
-	}()
-
-	<-exit
-	fmt.Println("Disconnected")
+	window.Resize(fyne.NewSize(600, 500))
+	window.SetContent(content)
+	window.ShowAndRun()
 }
