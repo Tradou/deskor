c9cd17f3f6b8564e3efe0676e85c854d754497fb
UrTeddyOnDrugs <taidoud@norsys.fr>
2023-11-07 17:05:30 +0100

    refactor(clean): create struct server

--- chat/chat.go
+++ chat/chat.go
@@ -1,8 +1,11 @@
 package chat
 
 import (
+	"encoding/json"
 	"net"
 )
+
+const serverName = "SERVER"
 
 type Message struct {
 	Sender    string `json:"sender"`
@@ -20,4 +23,66 @@ type Client struct {
 
 type Disconnect struct {
 	Client Client
+}
+
+type Server struct {
+	Clients   map[Client]bool
+	Join      chan Client
+	Leave     chan Disconnect
+	Messages  chan Message
+	Connected int
+}
+
+func NewServer() *Server {
+	return &Server{
+		Clients:  make(map[Client]bool),
+		Join:     make(chan Client),
+		Leave:    make(chan Disconnect),
+		Messages: make(chan Message),
+	}
+}
+
+func (s *Server) JoinClient(client Client) {
+	s.Join <- client
+}
+
+func (s *Server) LeaveClient(client Client) {
+	s.Leave <- Disconnect{Client: client}
+}
+
+func (s *Server) AddMessage(message Message) {
+	s.Messages <- message
+}
+
+func (s *Server) GetConnectedCount() int {
+	return s.Connected
+}
+
+func (s *Server) IncrementConnectedCount() {
+	s.Connected++
+}
+
+func (s *Server) DecrementConnectedCount() {
+	s.Connected--
+}
+
+func (s *Server) SendWelcomeMessage(client Client) error {
+	welcomeMessage := Message{
+		Sender:    serverName,
+		SenderIp:  "",
+		Text:      "Someone has arrived",
+		Connected: s.GetConnectedCount(),
+	}
+	welcomeMessageJSON, _ := json.Marshal(welcomeMessage)
+	_, err := client.Conn.Write(welcomeMessageJSON)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Server) Clean(client Client) {
+	delete(s.Clients, client)
+	s.LeaveClient(client)
+	client.Conn.Close()
 }
--- server.go
+++ server.go
@@ -8,19 +8,18 @@ import (
 	"log"
 )
 
-var clients = make(map[chat.Client]bool)
-var join = make(chan chat.Client)
-var leave = make(chan chat.Disconnect)
-var messages = make(chan chat.Message)
-var connected int
+var server *chat.Server
 
 func main() {
 	logger.New()
 	log.Print("Server has just started")
 
+	server := chat.NewServer()
+
 	listener, err := connect.Setup()
 	if err != nil {
 		log.Print(err)
+		return
 	}
 	defer listener.Close()
 
@@ -31,80 +30,56 @@ func main() {
 		if err != nil {
 			log.Printf("Error accepting connection: %s", err)
 		}
-		join <- client
+		server.JoinClient(client)
 	}
 }
 
 func broadcast() {
 	for {
 		select {
-		case client := <-join:
-			log.Printf("New client has joined: %s", client.Conn.RemoteAddr())
-			go func(client chat.Client) {
-				welcomeMessage := chat.Message{
-					Sender:    "SERVER",
-					SenderIp:  "",
-					Text:      "Someone has arrived",
-					Connected: connected,
-				}
-				welcomeMessageJSON, _ := json.Marshal(welcomeMessage)
-				_, err := client.Conn.Write(welcomeMessageJSON)
-				if err != nil {
-					log.Printf("Error while sending welcome message to %s : %s", client.Conn.RemoteAddr(), err)
-				}
-				connected++
-				go handleClient(client)
-			}(client)
-		case disconnect := <-leave:
-			connected--
-			log.Printf("Client left: %s", disconnect.Client.Conn.RemoteAddr())
-		case message := <-messages:
-			for client := range clients {
-				go func(c chat.Client, msg chat.Message) {
-					select {
-					case c.Messages <- msg:
-					default:
-						log.Print("Message not sent to clients")
-					}
-				}(client, message)
-			}
+		case client := <-server.Join:
+			handleJoin(client)
+		case disconnect := <-server.Leave:
+			handleLeave(disconnect.Client)
+		case message := <-server.Messages:
+			handleMessage(message)
 		}
 	}
 }
 
-func handleClient(client chat.Client) {
-	clients[client] = true
-	defer func() {
-		delete(clients, client)
-		leave <- chat.Disconnect{Client: client}
-		client.Conn.Close()
-	}()
+func handleLeave(client chat.Client) {
+	server.DecrementConnectedCount()
+	log.Printf("Client left: %s", client.Conn.RemoteAddr())
+}
 
-	go func() {
-		for message := range client.Messages {
-			msg := chat.Message{
-				Sender:    message.Sender,
-				SenderIp:  client.Conn.RemoteAddr().String(),
-				Text:      message.Text,
-				Connected: connected,
-			}
-
-			if chat.IsCommand(msg) {
-				msg = chat.Dispatch(msg)
-			}
-
-			messageJSON, err := json.Marshal(msg)
-			if err != nil {
-				log.Printf("Error sending message to client: %s", err)
-				return
-			}
-			_, err = client.Conn.Write(messageJSON)
-			if err != nil {
-				log.Printf("Error sending message to client: %s", err)
-				return
-			}
+func handleJoin(client chat.Client) {
+	log.Printf("New client has joined: %s", client.Conn.RemoteAddr())
+	go func(client chat.Client) {
+		if err := server.SendWelcomeMessage(client); err != nil {
+			log.Printf("Error while sending welcome message to %s : %s", client.Conn.RemoteAddr(), err)
 		}
-	}()
+		server.IncrementConnectedCount()
+		go handleClient(client)
+	}(client)
+}
+
+func handleMessage(message chat.Message) {
+	for client := range server.Clients {
+		go func(c chat.Client, msg chat.Message) {
+			select {
+			case c.Messages <- msg:
+			default:
+				log.Print("Message not sent to clients")
+			}
+		}(client, message)
+	}
+}
+
+func handleClient(client chat.Client) {
+	server.Clients[client] = true
+	defer server.Clean(client)
+
+	go sendClientMessages(client)
 
 	for {
 		message := make([]byte, 512)
@@ -116,9 +91,35 @@ func handleClient(client chat.Client) {
 
 		var chatMsg chat.Message
 		if err := json.Unmarshal([]byte(msg), &chatMsg); err == nil {
-			messages <- chatMsg
+			server.AddMessage(chatMsg)
 		} else {
 			log.Printf("Received invalid message: %s", err)
+		}
+	}
+}
+
+func sendClientMessages(client chat.Client) {
+	for message := range client.Messages {
+		msg := chat.Message{
+			Sender:    message.Sender,
+			SenderIp:  client.Conn.RemoteAddr().String(),
+			Text:      message.Text,
+			Connected: server.GetConnectedCount(),
+		}
+
+		if chat.IsCommand(msg) {
+			msg = chat.Dispatch(msg)
+		}
+
+		messageJSON, err := json.Marshal(msg)
+		if err != nil {
+			log.Printf("Error sending message to client: %s", err)
+			return
+		}
+		_, err = client.Conn.Write(messageJSON)
+		if err != nil {
+			log.Printf("Error sending message to client: %s", err)
+			return
 		}
 	}
 }
