588a80365e484828a05a090b6334994eefee2ab5
UrTeddyOnDrugs <taidoud@norsys.fr>
2023-11-09 16:49:07 +0100

    feat(command): retrieve command and flags

--- chat/helper.go
+++ chat/helper.go
@@ -1,13 +1,9 @@
 package chat
 
-import "strings"
-
-type CommandHelper interface {
-	IsCommand(msg Message) bool
-	isAnnouncement(msg Message) bool
-	ShouldBeEncrypt(msg Message) bool
-	ShouldBeDecrypt(msg Message) bool
-}
+import (
+	"regexp"
+	"strings"
+)
 
 func IsCommand(msg Message) bool {
 	return strings.HasPrefix(msg.Text, prefixCommand)
@@ -23,4 +19,23 @@ func ShouldBeEncrypt(msg Message) bool {
 
 func ShouldBeDecrypt(msg Message) bool {
 	return ShouldBeEncrypt(msg)
+}
+
+func getCommand(msg Message) string {
+	return msg.Text[len("a"):strings.Index(msg.Text, " ")]
+}
+
+func getFlags(msg Message) []string {
+	r := regexp.MustCompile(`--"([^"]*)"|--([^ ]*)`)
+	matches := r.FindAllStringSubmatch(msg.Text, -1)
+
+	var flags []string
+	for _, match := range matches {
+		if match[1] != "" {
+			flags = append(flags, match[1])
+		} else {
+			flags = append(flags, match[2])
+		}
+	}
+	return flags
 }
